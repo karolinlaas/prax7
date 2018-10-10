edd85c86a824caf39cd89263fea7f4f4ef2f5b7c
Karolin Laas <karolin.laas@khk.ee>
2018-10-10 12:42:51 +0300

    Lisatud tabeli näitamise ja peitmise funktsionaalsus

--- main.js
+++ main.js
@@ -3,10 +3,19 @@ let kgInput = document.getElementById('kgInput');
 let gramOutput = document.getElementById('gramOutput');
 let poundOutput = document.getElementById('poundOutput');
 let ozOutput = document.getElementById('ozOutput');
+let output = document.getElementById('output');
+
+output.style.visibility = 'hidden';
 
 // Listening to events
 kgInput.addEventListener('input', event => {    // addEventsListeneri sisse läheb kaks parameetrit, millest üks on function
     let kg = event.target.value;                // targetime sedasama elementi, kust event välja tuli, tahame sealt valuet kätte saada
+    
+    if (kg === '0') {                           // == tähedab, et on truthy, ilma ülakomadeta. === puhul '0' stringina
+        output.style.visibility = 'hidden';
+    } else {
+        output.style.visibility = 'visible';
+    }
     
     gramOutput.innerHTML = `${kg * 1000} g`;    // muudame teksti, mis ilmub html-is tagide vahele
     poundOutput.innerHTML = `${kg * 2.20462262} lb`;
