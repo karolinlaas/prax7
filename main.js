553fc7b92ddc985312ded9e523750c3e64af1ad3
Karolin Laas <karolin.laas@khk.ee>
2018-10-10 12:48:18 +0300

    If/else lause refaktoreerimine

--- main.js
+++ main.js
@@ -8,16 +8,12 @@ let output = document.getElementById('output');
 output.style.visibility = 'hidden';
 
 // Listening to events
-kgInput.addEventListener('input', event => {    // addEventsListeneri sisse läheb kaks parameetrit, millest üks on function
-    let kg = event.target.value;                // targetime sedasama elementi, kust event välja tuli, tahame sealt valuet kätte saada
+kgInput.addEventListener('input', event => {                        // addEventsListeneri sisse läheb kaks parameetrit, millest üks on function
+    let kg = event.target.value;                                    // targetime sedasama elementi, kust event välja tuli, tahame sealt valuet kätte saada
     
-    if (kg === '0') {                           // == tähedab, et on truthy, ilma ülakomadeta. === puhul '0' stringina
-        output.style.visibility = 'hidden';
-    } else {
-        output.style.visibility = 'visible';
-    }
+    output.style.visibility = (kg === '0') ? 'hidden' : 'visible';  // == tähedab, et on truthy, ilma ülakomadeta. ===
     
-    gramOutput.innerHTML = `${kg * 1000} g`;    // muudame teksti, mis ilmub html-is tagide vahele
+    gramOutput.innerHTML = `${kg * 1000} g`;                        // muudame teksti, mis ilmub html-is tagide vahele
     poundOutput.innerHTML = `${kg * 2.20462262} lb`;
     ozOutput.innerHTML = `${kg * 35.2739619} oz`;
 });
