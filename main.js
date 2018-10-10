3faa714d64452548dda53985cb3d577d05aff2f7
Karolin Laas <karolin.laas@khk.ee>
2018-10-10 11:34:58 +0300

    Lisatud grammide kalkuleerimise võimalus

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 // DOM elements
 let kgInput = document.getElementById('kgInput');
+let gramOutput = document.getElementById('gramOutput');
 
 // Listening to events
 kgInput.addEventListener('input', event => {    // addEventsListeneri sisse läheb kaks parameetrit, millest üks on function
     let kg = event.target.value;                // targetime sedasama elementi, kust event välja tuli, tahame sealt valuet kätte saada
-    console.log(kg);
+    
+    gramOutput.innerHTML = `${kg * 1000} g`;    // muudame teksti, mis ilmub html-is tagide vahele
 });
+
